72582e149e5171f7e22cfb290be03f690f6d6594
msidiq2235 <muhsidiq2005@gmail.com>
2024-06-13 19:38:54 +0700

    case4-5

--- Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
+++ Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
@@ -154,6 +154,19 @@ int main()
 			obj.preorder(obj.ROOT);
 			break;
 		}
+		case '4' :
+		{
+			obj.postorder(obj.ROOT);
+			break;
+		}
+		case '5' :
+			return 0;
+		default:
+		{
+			cout << "Invalid option" << endl;
+			break;
+		}
+
 		}
 	}
 }
