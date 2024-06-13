a5e77b928086d9548659185e0a5e175234f5cc74
msidiq2235 <muhsidiq2005@gmail.com>
2024-06-13 18:31:08 +0700

    5

--- Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
+++ Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
@@ -34,5 +34,10 @@ public:
 		newNode->info = element; // Assign value to the data fielf of the new node
 		newNode->leftchild = nullptr; // Make the left child of the new node point to NULL
 		newNode->rightchild = nullptr; // Make the right child of the new node point to NULL
+
+		Node* parent = nullptr;
+		Node* currentNode = nullptr;
+		search(element, parent, currentNode); //Locate the node which will be the parent od the node to be inserted
+
 	}
 };
