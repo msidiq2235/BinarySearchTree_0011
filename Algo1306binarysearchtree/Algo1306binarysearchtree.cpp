ade563c7f83cc38bbbbcf0da54030a96f2082156
msidiq2235 <muhsidiq2005@gmail.com>
2024-06-13 18:48:33 +0700

    void preorder

--- Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
+++ Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
@@ -84,4 +84,19 @@ public:
 			inorder(ptr->rightchild);
 		}
 	}
+
+	void preorder(Node* ptr)
+	{
+		if (ROOT == nullptr)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != nullptr)
+		{
+			cout << ptr->info << " ";
+			preorder(ptr->leftchild);
+			preorder(ptr->rightchild);
+		}
+	}
 };
