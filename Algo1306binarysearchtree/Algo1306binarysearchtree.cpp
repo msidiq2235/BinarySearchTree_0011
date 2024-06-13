f8a75f9220b555e5f739c9d5b787deb93b9d54ce
msidiq2235 <muhsidiq2005@gmail.com>
2024-06-13 18:20:54 +0700

    2

--- Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
+++ Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node
+{
+public:
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	//Constructor for the node Class
+	Node(string i, Node * l, Node* r)
+	{
+		info = i;
+		leftchild = l;
+		rightchild = r;
+	}
+};
