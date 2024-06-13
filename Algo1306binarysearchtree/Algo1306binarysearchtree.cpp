f699373a8c4f5ea780dbddebe438ebabb32c44cc
msidiq2235 <muhsidiq2005@gmail.com>
2024-06-13 18:52:54 +0700

    void postorder

--- Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
+++ Algo1306binarysearchtree/Algo1306binarysearchtree.cpp
@@ -99,4 +99,20 @@ public:
 			preorder(ptr->rightchild);
 		}
 	}
+
+	void postorder(Node* ptr)
+	{
+		//Performs the postordwr traversal of the tree
+		if (ROOT = nullptr)
+		{
+			cout << "Tree is empty" << endl;
+			return;
+		}
+		if (ptr != nullptr)
+		{
+			postorder(ptr->leftchild);
+			postorder(ptr->rightchild);
+			cout << ptr->info << " ";
+		}
+	}
 };
